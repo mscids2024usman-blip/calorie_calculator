e03eec7243948916b3070ea09284861bc5a1c5e4
USMAN KHAN <mscids2024.usman@unigoa.ac.in>
2025-10-29 04:33:23 +0530

    made changes in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv('food_data.csv')
-print(df.columns)
-print(df.head())
